bde6ece4e4564bacaa0fb3861d8a1bf7a9fc3d3d
zoudaifa <zoudaifa@didiglobal.com>
2021-03-05 21:36:56 +0800

    add file2db
    add file2db

---
+++ spring-batch-file2db/src/main/java/com/daleyzou/file2db/SpringBatchFile2dbApplication.java
@@ -0,0 +1,13 @@
+package com.daleyzou.file2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFile2dbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchFile2dbApplication.class, args);
+    }
+
+}
---
+++ spring-batch-file2db/src/test/java/com/daleyzou/file2db/SpringBatchFile2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daleyzou.file2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchFile2dbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
